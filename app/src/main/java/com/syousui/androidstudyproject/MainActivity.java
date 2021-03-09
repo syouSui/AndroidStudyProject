5d4c1d7a86735a1363ad77ca1fcac2e6ad8ae656
syouSui <justTouSan@gmail.com>
2021-03-09 20:00:38 +0800

    完成ch04的Activity生命周期.

--- app/src/main/java/com/syousui/androidstudyproject/MainActivity.java
+++ app/src/main/java/com/syousui/androidstudyproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.syousui.androidstudyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.syousui.androidstudyproject.ch04Activity.activitylifecycle.ActivityLifeCycle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, ActivityLifeCycle.class));
     }
 }
---
+++ app/src/main/java/com/syousui/androidstudyproject/ch04Activity/activitylifecycle/ActivityLifeCycle.java
@@ -0,0 +1,47 @@
+package com.syousui.androidstudyproject.ch04Activity.activitylifecycle;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.syousui.androidstudyproject.R;
+
+
+public class ActivityLifeCycle extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_life_cycle);
+        Log.e("ActivityLifeCycle","调用onCreate()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("ActivityLifeCycle", "调用onStart()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("ActivityLifeCycle", "调用onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("ActivityLifeCycle", "调用onPause()");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("ActivityLifeCycle", "调用onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("ActivityLifeCycle", "调用onDestroy()");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("ActivityLifeCycle", "调用onRestart()");
+    }
+}
